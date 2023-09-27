8c2de361a0abd4b0c56cc1f0aaa3c32141d5b51c
Diovane Alves <alvdiovane@gmail.com>
2023-09-27 15:36:46 -0300

    feat: add fonts and eslint dependencies and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: '#FF5100',
+        white: '#FFFFFF',
+        gray: '#C6C6C6',
+        background: '#282828',
+        semanticGreen: '#04D361',
+      },
+      fontFamily: {
+        regular: 'Epilogue_400Regular',
+        semibold: 'Epilogue_500Medium',
+        bold: 'Epilogue_700Bold',
+        extrabold: 'Epilogue_800ExtraBold',
+      },
+    },
   },
   plugins: [],
 }
-

bb3f258d8386ed999824089acae7597490140449
Diovane Alves <alvdiovane@gmail.com>
2023-09-30 15:10:41 -0300

    fix!: Main screen styling

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,35 +1,41 @@
-import { View, Text, FlatList } from 'react-native'
+import { ScrollView, Text, FlatList } from 'react-native'
 
 import { NavigationProps } from '../@types/navigation'
 import Card from '../components/Card'
 import { cardData } from '../utils/MockCardData'
-import MyButton from '../components/MyButton'
 
 export default function Main({ navigation }: NavigationProps<'Main'>) {
   return (
-    <View className="flex-1 pt-20 px-8 space-y-5 bg-background">
-      <Text className="text-3xl text-white font-bold">
-        Que tipo de evento você procura?
-      </Text>
-      <Text className="text-xl text-gray font-medium">
-        Selecione a categoria que mais te agrada!
-      </Text>
-
-      <FlatList
-        data={cardData}
-        numColumns={2}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Card
-            icon={item.icon}
-            title={item.title}
-            events={item.events}
-            style="mr-2 mb-2"
-          />
-        )}
-      />
-
-      <MyButton icon="arrow-right" iconSize={24} />
-    </View>
+    <FlatList
+      className="bg-background"
+      data={cardData}
+      numColumns={2}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <Card
+          icon={item.icon}
+          title={item.title}
+          events={item.events}
+          style="mb-2 mr-2"
+        />
+      )}
+      ListHeaderComponent={
+        <>
+          <Text className="text-3xl text-white font-bold mb-4">
+            Que tipo de evento você procura?
+          </Text>
+          <Text className="text-xl text-gray font-medium mb-6">
+            Selecione a categoria que mais te agrada!
+          </Text>
+        </>
+      }
+      contentContainerStyle={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 60,
+        paddingBottom: 20,
+      }}
+    />
   )
 }
